99527d4fef483628d240310372af34e4e8c376f4
StephaniGalindo <133808775+StephaniGalindo@users.noreply.github.com>
2023-07-19 10:12:14 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    ...nextConfig,
+    
+    env: {
+        ZAPATITOS_API_KEY: "9ydw2tlbi9ts5hrlcv7h1rrg",
+        ZAPATITOS_AUTH_DOMAIN: "dsm-3a-10e49.firebaseapp.com",
+        ZAPATITOS_PROJECT_ID: "dsm-3a-10e49",
+        ZAPATITOS_STORAGE_BUCKET: "dsm-3a-10e49.appspot.com",
+        ZAPATITOS_MESSAGING_SENDER_ID: "242093982308",
+        ZAPATITOS_APP_ID: "1:242093982308:web:ebb540ac29e8ecdea1d571"
+    },
+}
